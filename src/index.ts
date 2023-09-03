f6d5ec6e86ce7ede07251995929bc3df72963225
Sai Ranjit Tummalapalli <sairanjit5@gmail.com>
2023-09-03 07:44:59 +0530

    feat: add commander

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,41 @@
 #!/usr/bin/env node
 
-const hello = (message: string) => {}
+import { Command } from "commander"
+const program = new Command()
 
-console.log("Wow!!!!")
+program
+  .argument("<string>", "String to log")
+  .option("-c, --capitalize", "Capitalize the message")
+  .action(
+    (
+      message,
+      opts: {
+        capitalize?: boolean
+      }
+    ) => {
+      if (opts.capitalize) {
+        console.log(message.toUpperCase())
+      } else {
+        console.log(`Hello ${message}!!`)
+      }
+    }
+  )
+  .description("Say Hello")
+
+program
+  .command("add <numbers...>")
+  .action((numbers: number[]) => {
+    const total = numbers.reduce((a, b) => a + b, 0)
+    console.log(`Total: ${total}`)
+  })
+  .description("Add numbers and log the total")
+
+program
+  .command("get-max-numbers <numbers...>")
+  .action((numbers: number[]) => {
+    const max = Math.max(...numbers)
+    console.log(`Max: ${max}`)
+  })
+  .description("Add numbers and log the total")
+
+program.parse(process.argv)
